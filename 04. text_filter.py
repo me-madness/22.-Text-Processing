c25740b845cc4ddb533fc8cc8c10b16fe05a5250
me_madness <p_rangel@abv.bg>
2023-11-08 19:08:27 +0100

    04. Done by the lecture

--- 04. text_filter.py
+++ 04. text_filter.py
@@ -1,6 +1,12 @@
 # First task from the lecture
 
+ban_list_word = input().split()
+text = input()
 
+for word in ban_list_word:
+    text = text.replace(word, '*' * len(word))
+    
+print(text)    
 
 # Second task from me
 
