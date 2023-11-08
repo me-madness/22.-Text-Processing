3268e30c15f1c56f1895471e8a02758988fc15ee
me_madness <p_rangel@abv.bg>
2023-11-08 19:15:12 +0100

    05. Done by the lecture

--- 05. digits_letters_and_other.py
+++ 05. digits_letters_and_other.py
@@ -1,6 +1,23 @@
 # First task from the lecture
 
+text = input()
 
+numbers = ''
+strings = ''
+characters = ''
 
+for char in text:
+    if char.isdigit():
+        numbers += char
+    elif char.isalpha():
+        strings += char
+    else:
+        characters += char
+        
+        
+print(numbers)            
+print(strings)            
+print(characters)
+            
 # Second task from me
 
