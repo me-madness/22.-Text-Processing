65c0a908f3920439e5f96c6678495830fcc802e1
me_madness <p_rangel@abv.bg>
2023-11-08 19:04:11 +0100

    03. Done by the lecture

--- 03. substring.py
+++ 03. substring.py
@@ -1,6 +1,12 @@
 # First task from the lecture
 
+first_string = input()
+second_string = input()
 
+while first_string in second_string:
+    second_string = second_string.replace(first_string, '')
+    
 
+print(second_string)    
 # Second task from me
 
